6e6f03116493a700bd2bd216548af540d9254471
Rounak Biswal <rounakbiswal2003@gmail.com>
2025-09-11 14:29:15 +0530

    final commit before deployment

--- backend/Config/db.py
+++ backend/Config/db.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
-con = MongoClient("mongodb://127.0.0.1:27017")
+# con = MongoClient("mongodb://127.0.0.1:27017")
+con = MongoClient("mongodb+srv://RounakBiswal:RounakClusterPassword2003@rounak-project-cluster.cbl2tbz.mongodb.net/LMS?retryWrites=true&w=majority&appName=Rounak-project-cluster")
 database = con["LMS"]
 collection = database["leaves"]
