5735c0cdf7dcd485ee8b59cc801496db045b6ee6
Rounak Biswal <rounakbiswal2003@gmail.com>
2025-09-12 18:19:41 +0530

    deply issue fixing

--- backend/index.py
+++ backend/index.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from Routes.leave_route import leave
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+import os  # we’ll use this in the debug route
 
 app = FastAPI()
 
@@ -21,4 +22,15 @@ app.add_middleware(
 
 # app.mount("/Static", StaticFiles(directory="Static"), name="Static")
 templates = Jinja2Templates(directory="templates")
+
+# ✅ Debug route to see what’s actually deployed
+@app.get("/debug-list")
+async def debug_list(request: Request):
+    return {
+        "cwd": os.getcwd(),
+        "templates_dir": os.path.join(os.getcwd(), "templates"),
+        "files_in_templates": os.listdir("templates") if os.path.exists("templates") else "no templates folder"
+    }
+
+# include your other routers after
 app.include_router(leave)
